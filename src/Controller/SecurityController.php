44c35e3bda215873ab34ababae630ba30c68e593
bdomart <bdomart@normandiewebschool.fr>
2019-11-16 16:24:48 +0100

    Work in progress - Log and Register : - JSON login endpoint added - Register method added - Annotation updated with FOSRestBundle

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\View\View;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class SecurityController extends AbstractController
+/**
+ * Class SecurityController
+ * @package App\Controller
+ * @Rest\Route("/api")
+ */
+class SecurityController extends AbstractFOSRestController
 {
     /**
-     * @Route("/login", name="app_login")
+     * @Rest\POST("login", name="app_login")
+     * @param AuthenticationUtils $authenticationUtils
+     * @param Request $request
+     * @return View
      */
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, Request $request): View
     {
         // if ($this->getUser()) {
         //    $this->redirectToRoute('target_path');
@@ -23,14 +37,46 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return View::create([
+            'error' => $error,
+            'lastUsername' => $lastUsername
+        ], Response::HTTP_OK);
     }
 
     /**
-     * @Route("/logout", name="app_logout")
+     * @Rest\POST("/logout", name="app_logout")
      */
     public function logout()
     {
         throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
+    }
+
+    /**
+     * @Route\POST("/register", name="app_register")
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @param EntityManagerInterface $em
+     * @return View
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, EntityManagerInterface $em): View
+    {
+        $user = new User();
+
+        if (empty($request->request->get("email"))
+            || empty($request->request->get("password"))) {
+            return View::create(['error' => 'Email or password cannot be empty'], Response::HTTP_BAD_REQUEST);
+        }
+        if ($em->getRepository(User::class)->findOneBy(["email" => $request->request->get("email")])) {
+            return View::create(['error' => 'Email already exist'], Response::HTTP_BAD_REQUEST);
+        }
+
+        $user->setEmail($request->request->get("email"));
+        $password = $passwordEncoder->encodePassword($user, $request->request->get("password"));
+        $user->setPassword($password);
+
+        $em->persist($user);
+        $em->flush();
+
+        return View::create($user, Response::HTTP_CREATED);
     }
 }
