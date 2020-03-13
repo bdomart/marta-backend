7583f2f1dcafbdee89069947bc58cea176bdb3f8
bdomart <bdomart@normandiewebschool.fr>
2020-03-13 15:19:44 +0100

    Work in progress : - Login with token - method navigation & homepage

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,18 +2,62 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Site;
+use App\Entity\Subscription;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\View\View;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends AbstractController
+class DefaultController extends AbstractFOSRestController
 {
     /**
-     * @Route("/", name="app_homepage")
+     * @Rest\Get("/", name="app_homepage")
+     * @return View
      */
     public function index()
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
+        return View::create([], Response::HTTP_OK);
+    }
+
+    /**
+     * @Rest\Get("/navigation", name="app_navigation")
+     * @param Request $request
+     * @return View
+     */
+    public function navigation(Request $request)
+    {
+        $departments = [];
+        if ($request->query->get('regions')) {
+            $departments = explode(',', $request->query->get('departments'));
+        } elseif ($this->getUser()) {
+            $user_adresses = $this->getUser()->getAdresses();
+            $departments = [];
+        }
+
+        $tags = [];
+        if ($request->query->get('tags')) {
+            $tags = explode(',', $request->query->get('tags'));
+        }
+
+        $sites = $this->getDoctrine()
+            ->getRepository(Site::class)
+            ->findSitesByDepartmentOrTag($departments, $tags);
+
+        return View::create($sites, Response::HTTP_OK);
+    }
+
+    /**
+     * @Rest\Get("/presentation", name="app_presentation")
+     * @return View
+     */
+    public function presentation()
+    {
+        $subscriptions = $this->getDoctrine()
+            ->getRepository(Subscription::class)
+            ->findAll();
+
+        return View::create($subscriptions, Response::HTTP_OK);
     }
 }
